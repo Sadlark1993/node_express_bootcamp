dff1a44bd10187b4860d11b838b780d93f606a56
Claudio Zimmermann Junior <claudio.zi.junior@gmail.com>
2025-01-01 20:07:52 -0400

    routing

--- 1-node-farm/starter/server.js
+++ 1-node-farm/starter/server.js
@@ -3,7 +3,18 @@ const url = require('url');
 
 const server = http.createServer((req, res) => {
   //console.log(req);
-  res.end('Hello from the server!');
+  const pathName = req.url;
+  if (pathName === '/' || pathName === '/overview') {
+    res.end('This is the overview page.');
+  } else if (pathName === '/product') {
+    res.end('This is the product page.');
+  } else {
+    res.writeHead(404, {
+      'Content-type': 'text/html',
+      'my-own-header': 'hello-world',
+    });
+    res.end('<h1>404 page not found.</h1>');
+  }
 });
 
 server.listen(8000, () => {
