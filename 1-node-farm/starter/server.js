0412b9c16d1401a6165b84a8dd6e5b6a3cce8a3a
Claudio Zimmermann Junior <claudio.zi.junior@gmail.com>
2025-01-01 21:45:43 -0400

    queries

--- 1-node-farm/starter/server.js
+++ 1-node-farm/starter/server.js
@@ -1,20 +1,41 @@
+const fs = require('fs');
 const http = require('http');
 const url = require('url');
 
+//it will run only once, so it can be sync
+const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, 'utf-8');
+const dataObj = JSON.parse(data);
+
 const server = http.createServer((req, res) => {
   //console.log(req);
-  const pathName = req.url;
-  if (pathName === '/' || pathName === '/overview') {
-    res.end('This is the overview page.');
-  } else if (pathName === '/product') {
-    res.end('This is the product page.');
-  } else {
-    res.writeHead(404, {
-      'Content-type': 'text/html',
-      'my-own-header': 'hello-world',
-    });
-    res.end('<h1>404 page not found.</h1>');
+  const { query, pathname } = url.parse(req.url, true);
+
+  if (pathname === '/' || pathname === '/overview') {
+    return res.end('This is the overview page.');
   }
+
+  if (pathname === '/product') {
+    if (typeof query.id !== 'undefined') {
+      console.log('query: ');
+      console.log(query);
+      const product = query.id;
+      return res.end(`This is the page of the product ${product}.`);
+    }
+  }
+
+  if (pathname === '/api') {
+    res.writeHead(200, {
+      'Content-type': 'application/json',
+    });
+    return res.end(data);
+  }
+
+  //404
+  res.writeHead(404, {
+    'Content-type': 'text/html',
+    'my-own-header': 'hello-world',
+  });
+  res.end('<h1>404 page not found.</h1>');
 });
 
 server.listen(8000, () => {
