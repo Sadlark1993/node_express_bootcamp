d20c9cf43fe93e0d8e2182079284037b2aff5141
Claudio Zimmermann Junior <claudio.zi.junior@gmail.com>
2025-01-04 18:36:51 -0400

    firts GET method

--- 4-natours/starter/app.js
+++ 4-natours/starter/app.js
@@ -1,9 +1,27 @@
-const express = require('express');
+import fs from 'fs';
+import { fileURLToPath } from 'url';
+import path from 'path';
+import express from 'express';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const app = express();
 
-app.get('/', (req, res) => {
-  res.status(200).json({ message: 'Hello from the server side!', app: 'Natours' });
+// app.get('/', (req, res) => {
+//   res.status(200).json({ message: 'Hello from the server side!', app: 'Natours' });
+// });
+
+const tours = JSON.parse(fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`));
+
+app.get('/api/v1/tours', (req, res) => {
+  res.status(200).json({
+    status: 'success',
+    results: tours.length,
+    data: {
+      tours,
+    },
+  });
 });
 
 const port = 3000;
