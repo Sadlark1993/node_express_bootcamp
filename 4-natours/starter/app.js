dcf5c70fadb7df7cf094efab7427cbb65f12923e
Claudio Zimmermann Junior <claudio.zi.junior@gmail.com>
2025-01-05 02:21:53 -0400

    Adding the first middleware

--- 4-natours/starter/app.js
+++ 4-natours/starter/app.js
@@ -11,11 +11,23 @@ const app = express();
 //this middleware grant us access to the body of the incoming req
 app.use(express.json());
 
+app.use((req, res, next) => {
+  console.log('Hallo from the middleware 👋');
+  next();
+});
+
+app.use((req, res, next) => {
+  req.requestTime = new Date().toISOString();
+  next();
+});
+
 const tours = JSON.parse(fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`));
 
 const getAllTours = (req, res) => {
+  console.log(req.requestTime);
   res.status(200).json({
     status: 'success',
+    requestedAt: req.requestTime,
     results: tours.length,
     data: {
       tours,
@@ -70,12 +82,6 @@ const deleteTour = (req, res) => {
     message: 'tour deleted',
   });
 };
-
-// app.get('/api/v1/tours', getAllTours);
-// app.post('/api/v1/tours', createTour);
-// app.get('/api/v1/tours/:id', getTour);
-// app.patch('/api/v1/tours/:id', updateTour);
-// app.delete('/api/v1/tours/:id', deleteTour);
 
 app.route('/api/v1/tours').get(getAllTours).post(createTour);
 
