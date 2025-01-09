d1e33c29eeeea84e65d2263dba75e8aad3225b2c
Claudio Zimmermann Junior <Claudio.zi.junior@gmail.com>
2025-01-09 14:15:42 -0400

    Serving static files in express

--- 4-natours/starter/app.js
+++ 4-natours/starter/app.js
@@ -2,6 +2,10 @@ import express from 'express';
 import morgan from 'morgan';
 import tourRouter from './routes/tourRoutes.js';
 import userRouter from './routes/userRoutes.js';
+import { fileURLToPath } from 'url';
+import path from 'path';
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const app = express();
 
@@ -9,8 +13,11 @@ const app = express();
 
 app.use(morgan('dev'));
 
-//this middleware grant us access to the body of the incoming req
+// this middleware grant us access to the body of the incoming req
 app.use(express.json());
+
+// this is just to show how to serve static files
+app.use(express.static(`${__dirname}/public`));
 
 app.use((req, res, next) => {
   console.log('Hallo from the middleware 👋');
